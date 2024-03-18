74e145bc459b758c0a7bd53582cac4fc3390bfd4
shake863 <shake863@gmail.com>
2024-03-18 13:31:21 +0800

    add log for check

--- src/addFeedItems.ts
+++ src/addFeedItems.ts
@@ -12,6 +12,9 @@ export const addFeedItems = async (
 ) => {
   const notion = new Client({ auth: process.env.NOTION_KEY })
   const databaseId = process.env.NOTION_READER_DATABASE_ID || ''
+  console.log('addFeedItems notion_key:', process.env.NOTION_KEY)
+  console.log('addFeedItems databaseId:', databaseId)
+
   console.log('newFeedItems.length: ', newFeedItems.length)
   newFeedItems.forEach(async (item) => {
     const { title, link, enclosure, pubDate } = item
--- src/getFeedUrlList.ts
+++ src/getFeedUrlList.ts
@@ -5,8 +5,9 @@ type TODO = any
 
 export const getFeedUrlList = async () => {
   const notion = new Client({ auth: process.env.NOTION_KEY })
+  console.log('getFeedUrlList notion_key:', process.env.NOTION_KEY)
   const databaseId = process.env.NOTION_FEEDER_DATABASE_ID || ''
-
+  console.log('getFeedUrlList databaseId:', databaseId)
   const response = await notion.databases.query({
     database_id: databaseId,
   })
